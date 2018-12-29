477d6982a05d35b8cef0d94a8141e34b74df97df
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2018-12-30 01:12:18 +0400

    x

--- r2dbchero/src/main/java/io/project/app/r2dbchero/resources/HeroResource.java
+++ r2dbchero/src/main/java/io/project/app/r2dbchero/resources/HeroResource.java
@@ -3,6 +3,8 @@ package io.project.app.r2dbchero.resources;
 import io.project.app.r2dbchero.domain.User;
 import io.project.app.r2dbchero.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +14,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.ServerResponse.notFound;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import org.springframework.http.ResponseEntity;
+import static org.springframework.web.reactive.function.BodyInserters.fromPublisher;
 
 /**
  *
@@ -55,5 +65,15 @@ public class HeroResource {
     public Mono<Void> delete(@RequestParam(required = true, name = "id") Long id) {
         return userService.deleteById(id);
     }
+
+    @GetMapping("/tweets/{id}")
+    public Mono<ResponseEntity<User>> load(@RequestParam(required = true, name = "id") Long id) {
+        return userService.findById(id)
+                .map(savedTweet -> ResponseEntity.ok(savedTweet))
+                .defaultIfEmpty(ResponseEntity.notFound().build());
+    }
+    
+    
+    // https://github.com/callicoder/spring-webflux-reactive-rest-api-demo/blob/master/src/main/java/com/example/webfluxdemo/controller/TweetController.java
 
 }
