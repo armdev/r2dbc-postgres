ed19e7543bc48be24cd1329e2e188ddcb12cf163
Armen Arzumanyan <armena@YLT160015.CORP.OPTYM.NET>
2019-01-19 14:28:41 +0400

    next push

--- r2dbchero/src/main/java/io/project/app/r2dbchero/R2dbcheroApplication.java
+++ r2dbchero/src/main/java/io/project/app/r2dbchero/R2dbcheroApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
 
